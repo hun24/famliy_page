fc12fe95f673d9853eb3a028ed840efa6152f8f8
hun24 <100418580+hun24@users.noreply.github.com>
2024-01-02 22:13:34 +0900

    Add files via upload

---
+++ event.js
@@ -0,0 +1,44 @@
+const event1 = {
+    name:"설날 이벤트 '새해 복 많이 받으세요!'",
+    intro:"설날을 즐겨라~",
+    date:"2024/02/09 ~ 2024/02/12"
+}
+document.getElementById("data1-1").innerHTML=event1["name"];
+document.getElementById("data1-2").innerHTML=event1["intro"];
+document.getElementById("data1-3").innerHTML=event1["date"];
+
+const event2 = {
+    name:"준비중 입니다!",
+    intro:"준비중 입니다!",
+    date:"20??/??/?? ~ 20??/??/??"
+}
+document.getElementById("data2-1").innerHTML=event2["name"];
+document.getElementById("data2-2").innerHTML=event2["intro"];
+document.getElementById("data2-3").innerHTML=event2["date"];
+
+const event3 = {
+    name:"준비중 입니다!",
+    intro:"준비중 입니다!",
+    date:"20??/??/?? ~ 20??/??/??"
+}
+document.getElementById("data3-1").innerHTML=event3["name"];
+document.getElementById("data3-2").innerHTML=event3["intro"];
+document.getElementById("data3-3").innerHTML=event3["date"];
+
+const event4 = {
+    name:"준비중 입니다!",
+    intro:"준비중 입니다!",
+    date:"20??/??/?? ~ 20??/??/??"
+}
+document.getElementById("data4-1").innerHTML=event4["name"];
+document.getElementById("data4-2").innerHTML=event4["intro"];
+document.getElementById("data4-3").innerHTML=event4["date"];
+
+const event5 = {
+    name:"준비중 입니다!",
+    intro:"준비중 입니다!",
+    date:"20??/??/?? ~ 20??/??/??"
+}
+document.getElementById("data5-1").innerHTML=event5["name"];
+document.getElementById("data5-2").innerHTML=event5["intro"];
+document.getElementById("data5-3").innerHTML=event5["date"];
